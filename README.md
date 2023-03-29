9e5638933e8863c4fd5d3a850f7831f6c6d3433e
Markus Muschol <markus.muschol@denkwerk.com>
2023-03-29 06:51:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# First steps in OPEN AI
+
+Learning project to try out some Open AI stuff with the nodejs SDK.
+
+To install grab a API Token from Open AI Website
+https://platform.openai.com/account/api-keys
+
+Run code
+```
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const response = openai
+  .createCompletion({
+    model: "text-davinci-003",
+    prompt: "Erzähl mir einen lustigen Witz",
+    temperature: 0,
+    max_tokens: 100,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  })
+  .then((response) => {
+    console.log(response.data.choices.map(({ text }) => text).join());
+  });
